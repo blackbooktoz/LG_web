634f2006427d8cabb1bbded18546e3489f507718
Black Book <blackbook666@proton.me>
2025-07-28 09:42:01 -0700

    m

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753663202|865208749';
+const CACHE_VERSION = '1753720873|41858596';
 /** @type {string} */
 const CACHE_PREFIX = "Lilith's Guidanc-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
